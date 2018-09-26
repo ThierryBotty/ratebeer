86939d7de5c9a35ed511e5a253c7552b7236e4b0
root <root@Sorkkaelin4.localdomain>
2018-09-27 00:34:32 +0300

    Viikko2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'breweries#index'
   resources :beers
   resources :breweries
+  get 'kaikki_bisset', to: 'beers#index'
+  resources :ratings, only: [:index, :new, :create]
+  resources :ratings, only: [:index, :new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
