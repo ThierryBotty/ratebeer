902aa27e925d462c705cf7959963fa569d0dcc45
Thierry Botty <thierry.botty@helsinki.fi>
2018-10-15 01:19:07 +0300

    fixed creating breweries and minor code rubocopping

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,6 +1,7 @@
 class BreweriesController < ApplicationController
-  before_action :ensure_that_signed_in, except: [:index, :show]
   before_action :set_brewery, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_that_signed_in, except: [:index, :show]
+  before_action :current_user_admin, only: [:destroy]
 
   # GET /breweries
   # GET /breweries.json
@@ -65,7 +66,7 @@ class BreweriesController < ApplicationController
 
   def toggle_activity
     brewery = Brewery.find(params[:id])
-    brewery.update_attribute :active, (!brewery.active)
+    brewery.update_attribute :active, !brewery.active
 
     new_status = brewery.active? ? "active" : "retired"
 
@@ -74,12 +75,10 @@ class BreweriesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_brewery
     @brewery = Brewery.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def brewery_params
     params.require(:brewery).permit(:name, :year, :active)
   end
