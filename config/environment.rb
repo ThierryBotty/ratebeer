4364bee14ca29c46c08910e82222cc7a5c9f7167
Thierry Botty <thierry.botty@helsinki.fi>
2018-10-22 17:58:08 +0300

    try over from scratch

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Capybara.ignore_hidden_elements = false
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,7 +34,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.assets.js_compressor = Uglifier.new(harmony: true)
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
